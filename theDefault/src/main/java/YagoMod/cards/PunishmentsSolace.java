48ef869ad8de4e6a850029cb8b2dfb9da894c8c3
YAGOTAGO <tiagocaetanodavies@gmail.com>
2022-11-26 01:07:48 -0500

    PunishmentsSolace Added.

---
+++ theDefault/src/main/java/YagoMod/cards/PunishmentsSolace.java
@@ -0,0 +1,61 @@
+package YagoMod.cards;
+
+import YagoMod.DefaultMod;
+import YagoMod.characters.TheDefault;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.utility.DiscardToHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static YagoMod.DefaultMod.makeCardPath;
+
+public class PunishmentsSolace extends AbstractDynamicCard {
+    /*
+     * (0): Deal 5 damage. NL whenever you take damage return this from the discard pile to your Hand.
+     */
+
+    public static final String ID = DefaultMod.makeID(PunishmentsSolace.class.getSimpleName());
+    public static final String IMG = makeCardPath("Attack.png");
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 0;
+    private static final int DAMAGE = 5;
+    private static final int UPGRADE_PLUS_DMG = 2;
+
+    public PunishmentsSolace() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        this.baseDamage = DAMAGE;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+
+    }
+
+    @Override
+    public void tookDamage() {
+        AbstractDungeon.actionManager.addToBottom(new DiscardToHandAction(this));
+    }
+
+    public AbstractCard makeCopy() {return new PunishmentsSolace();}
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            initializeDescription();
+        }
+    }
+}
