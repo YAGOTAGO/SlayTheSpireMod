cc166f99c324a2991dbee4a861ba2c52498a58c7
YAGOTAGO <tiagocaetanodavies@gmail.com>
2022-11-30 23:01:38 -0500

    RighteousCause Added.

---
+++ theDefault/src/main/java/YagoMod/cards/RighteousCause.java
@@ -0,0 +1,62 @@
+package YagoMod.cards;
+
+import YagoMod.DefaultMod;
+import YagoMod.characters.TheDefault;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
+
+import static YagoMod.DefaultMod.makeCardPath;
+
+public class RighteousCause extends AbstractDynamicCard {
+    /*
+     * (0): Deal 8(11) damage. NL Discard 1 card.
+     */
+
+    public static final String ID = DefaultMod.makeID(RighteousCause.class.getSimpleName());
+    public static final String IMG = makeCardPath("Attack.png");
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 0;
+    private static final int DAMAGE = 8;
+    private static final int UPGRADE_PLUS_DMG = 3;
+
+
+    public RighteousCause() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        this.baseDamage = DAMAGE;
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+
+        AbstractDungeon.actionManager.addToBottom(new DiscardAction(p, p, 1, false));
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+
+    }
+
+    //used by other methods to add a copy to the hand
+    public AbstractCard makeCopy() {return new RighteousCause();}
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            initializeDescription();
+        }
+    }
+}
--- theDefault/src/main/java/YagoMod/characters/TheDefault.java
+++ theDefault/src/main/java/YagoMod/characters/TheDefault.java
@@ -167,7 +167,7 @@ public class TheDefault extends CustomPlayer {
         retVal.add(Gash.ID);
         retVal.add(SinnersBlood.ID);
         retVal.add(SelfReflection.ID);
-        retVal.add(FairyBrew.ID);
+        retVal.add(RighteousCause.ID);
         retVal.add(WickedStab.ID);
         retVal.add(Offering.ID);
         retVal.add(Offering.ID);
