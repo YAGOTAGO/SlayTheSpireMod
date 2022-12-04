952e0ff357fb24219fc0b8db534d28da8c743ec8
YAGOTAGO <tiagocaetanodavies@gmail.com>
2022-12-03 22:04:31 -0500

    Basic Cards Added.

---
+++ theDefault/src/main/java/YagoMod/cards/Defend.java
@@ -0,0 +1,52 @@
+package YagoMod.cards;
+
+import YagoMod.DefaultMod;
+import YagoMod.characters.TheDefault;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static YagoMod.DefaultMod.makeCardPath;
+
+public class Defend extends AbstractDynamicCard {
+
+    /*
+     * (1): Gain 5(8) Block.
+     */
+
+    public static final String ID = DefaultMod.makeID(Defend.class.getSimpleName());
+    public static final String IMG = makeCardPath("Defend.png");
+    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int BLOCK = 5;
+    private static final int BLOCK_PLUS = 3;
+
+    public Defend() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        this.baseMagicNumber = BLOCK;
+        this.magicNumber = this.baseMagicNumber;
+        this.tags.add(CardTags.STARTER_DEFEND);
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, magicNumber));
+
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(BLOCK_PLUS);
+            initializeDescription();
+        }
+    }
+}
---
+++ theDefault/src/main/java/YagoMod/cards/Strike.java
@@ -0,0 +1,57 @@
+package YagoMod.cards;
+
+import YagoMod.DefaultMod;
+import YagoMod.characters.TheDefault;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+
+import static YagoMod.DefaultMod.makeCardPath;
+
+
+public class Strike extends AbstractDynamicCard {
+    /*
+     * (1): Deal 6(9) damage.
+     */
+
+    public static final String ID = DefaultMod.makeID(Strike.class.getSimpleName());
+    public static final String IMG = makeCardPath("Strike.png");
+    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 6;
+    private static final int UPGRADE_PLUS_DMG = 3;
+
+    public Strike() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        this.baseDamage = DAMAGE;
+        this.tags.add(CardTags.STRIKE);
+        this.tags.add(CardTags.STARTER_STRIKE);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+    }
+
+    //used by other methods to add a copy to the hand
+    public AbstractCard makeCopy() {return new Strike();}
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            initializeDescription();
+        }
+    }
+}
